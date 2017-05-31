753fd790f29de4c5c68297840b9d6431ef9c4917
stephan lindauer <s.lindauer@rebuy.com>
2017-05-31 23:16:43 +0200

    fix

--- routes/gallery.js
+++ routes/gallery.js
@@ -28,6 +28,9 @@ var saveImage = function(req, res, next) {
   var newWidth = maxWidth;
   var newHeight = Math.floor(oldHeight * (maxWidth / oldWidth));
 
+// todo  WARNING: Creating a duplicate QueryFile object for the same file -
+
+
   gm(req.file.path)
     .resize(newWidth, newHeight)
     .noProfile()
@@ -53,8 +56,8 @@ var saveImage = function(req, res, next) {
     });
 }
 
-router.post('/', upload.single('uploaded_image'), saveImage);
-router.post('/post.php', upload.single('uploaded_image'), saveImage);
+router.post('/', upload.single('uploaded_file'), saveImage);
+router.post('/post.php', upload.single('uploaded_file'), saveImage);
 
 router.get('/', function(req, res, next) {
   postgres_db.any("SELECT id, longitude, latitude FROM gallery")
@@ -86,50 +89,3 @@ router.get('/image/:id', function(req, res, next) {
 
 
 module.exports = router;
-
-// <?php
-//
-// //save file
-// $imageId = uniqid();
-// $filename = "./images/" . $imageId . ".jpg";
-// $filename_thumb = "./images/" . $imageId . "_thumb.jpg";
-//
-// if (!move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $filename)) {
-// 	die("fail: moving file");
-// }
-//
-//
-// //create thumbnail
-// $final_width_of_image = 100;
-//
-// $image = imagecreatefromjpeg($filename);
-//
-// $oldWidth = imagesx($image);
-// $oldHeight = imagesy($image);
-//
-// $newWidth = $final_width_of_image;
-// $newHeight = floor($oldHeight * ($final_width_of_image / $oldWidth));
-//
-// $newImage = imagecreatetruecolor($newWidth, $newHeight);
-//
-// imagecopyresized($newImage, $image, 0, 0, 0, 0, $newWidth, $newHeight, $oldWidth, $oldHeight);
-//
-// imagejpeg($newImage, $filename_thumb);
-//
-//
-// //save to database
-// $jsonObject = json_decode($_POST["data"]);
-//
-// $longitude = filter_var($jsonObject -> longitude, FILTER_SANITIZE_STRING);
-// $latitude = filter_var($jsonObject -> latitude, FILTER_SANITIZE_STRING);
-//
-//
-// if (isset($longitude) && isset($latitude) && isset($imageId)) {
-// 	$locationObject = $jsonObject -> location;
-// 	$query = "INSERT INTO `db539887603`.`Images` (`id`, `timestamp`, `longitude`, `latitude`, `imageId` ) VALUES (NULL, CURRENT_TIMESTAMP,'" . $longitude . "', '" . $latitude . "', '" . $imageId . "' );";
-// 	mysqli_query($con, $query);
-// }
-//
-// mysqli_close($con);
-//
-// echo "success";
